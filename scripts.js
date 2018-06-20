ad84c0d0dac0df1d05195f5484db87618f8c975d
verdantpath <m_dittmann@yahoo.com>
2018-06-19 23:21:51 -0700

    add scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+jQuery(function() {
+  var vegetarian = false;
+  jQuery('button#vegOn').on('click', function() {
+    if (vegetarian == false) {
+      vegetarian = true;
+      console.log(vegetarian);
+    }
+  }); // end button
+  jQuery('button#restoreMe').on('click', function() {
+    if (vegetarian == true) {
+      vegetarian = false;
+      console.log(vegetarian);
+    }
+  });
+});
