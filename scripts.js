174fd4d0e251ededa96fe6554b0f4b5960edd559
verdantpath <m_dittmann@yahoo.com>
2018-07-01 22:15:40 -0700

    update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ jQuery(function() {
       console.log(vegetarian);
       $f = $('li.fish').parent().parent().detach();
       $('.hamburger').replaceWith('<li class="portabello"><em>Portabello Mushroom</em></li>');
+      $('.meat').after('<li class="tofu">Tofu</li>');
+      $m = $('li.meat').detach();
     }
   }); // end button
   jQuery('button#restoreMe').on('click', function() {
